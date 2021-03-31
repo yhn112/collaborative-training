313e7f0afffa3bedc93bbe5988605b144bb9576a
Michael Diskin <yhn1124@gmail.com>
2021-03-31 08:50:07 +0300

    clip

--- collaboration.py
+++ collaboration.py
@@ -133,6 +133,9 @@ class CollaborativeTrainer(ExtendableTrainer):
                 self.dht.store(self.my_progess_key, subkey=self.trainer_uuid, value=my_info,
                                expiration_time=hivemind.get_dht_time() + self.collaboration_args.statistics_expiration,
                                return_future=True)
+                
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+
 
                 average_tr_loss = self.averager_step(tr_loss)
                 tr_loss = self.optimizer_step(epoch, step, average_tr_loss, trial, steps_in_epoch)
